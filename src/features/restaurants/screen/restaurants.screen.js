00cb7206baac4ad7bbdb6ba68e76e6b5e9811a5b
sohail005 <75928841+sohail005@users.noreply.github.com>
2021-09-05 14:40:43 +0530

    added multiple items to  the app

--- src/features/restaurants/screen/restaurants.screen.js
+++ src/features/restaurants/screen/restaurants.screen.js
@@ -1,31 +1,57 @@
-import React from "react";
-import {StatusBar,StyleSheet, Text, View, SafeAreaView,Platform } from 'react-native';
+import * as React from "react";
+import {FlatList, SafeAreaView} from 'react-native';
 import { Searchbar } from 'react-native-paper';
 import {RestaurantInfoCard} from '../components/restaurant-info.Card.component';
 import styled from "styled-components/native";
+import { Spacer } from "../../../components/spacer/spacer.component";
+import {SafeArea} from "../../../utility/safe-area.component";
 
-const SafeArea = styled(SafeAreaView)`
-flex: 1;
- ${StatusBar.currentHeight && `margin-top:${StatusBar.currentHeight}px`};
-`;
+
 const SerchContainer = styled(SafeAreaView)`
-padding: 16px;
+padding: 10px;
  backgroundColor:lightgray;
 `;
 const RestaurantListContainer = styled(SafeAreaView)`
-flex: 1;
  padding: 16px;
   backgroundColor:lightgray;
 `;
+const RestaurantList = styled(FlatList).attrs({
+  contentContainerStyle: {
+    padding: 10,
+  },
+})`margin-bottom: ${(props) => props.theme.space[2]};backgroundColor:lightgray`;
+
 
 export const RestaurantsScreen =()=>(
     <SafeArea >
     <SerchContainer >
-      <Searchbar />
+      <Searchbar  placeholder="Search"/>
     </SerchContainer> 
-    <RestaurantListContainer >
-      <RestaurantInfoCard />
-    </RestaurantListContainer>
+    <RestaurantList 
+    data={[
+      {name:1}, 
+      {name:2},
+       {name:3},
+        {name:4},
+        {name:5},
+        {name:6},
+         {name:7},
+         {name:8},
+       {name:9},
+        {name:10},
+        {name:11},
+        {name:12},
+         {name:13},
+    ]}
+    renderItem={() => (
+      <Spacer position="bottom" size="large">
+        <RestaurantInfoCard />
+      </Spacer>
+    )}
+    keyExtractor={(item)=>item.name}
+    //contentContainerStyle={{padding: 16, marginTop:5}}
+    />
+    
     </SafeArea>
 );
 
