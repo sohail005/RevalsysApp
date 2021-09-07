c2a786f0be2c396bbf92764c4ef65648e6afffbc
sohail005 <75928841+sohail005@users.noreply.github.com>
2021-09-07 12:09:12 +0530

    installed camelize in json

--- src/features/restaurants/components/restaurant-info.Card.component.js
+++ src/features/restaurants/components/restaurant-info.Card.component.js
@@ -24,9 +24,9 @@ export const RestaurantInfoCard = ({ restaurant = {} }) => {
       "https://www.foodiesfeed.com/wp-content/uploads/2019/06/top-view-for-box-of-2-burgers-home-made-600x899.jpg",
     ],
     address = "100 some random street",
-    isOpenNow = true,
+    isOpenNow = false,
     rating = 5,
-    isClosedTemporarily = false,
+    isClosedTemporarily = true,
   } = restaurant;
 
   //intialising rating
