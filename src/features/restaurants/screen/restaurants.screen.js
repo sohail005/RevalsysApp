80b47b9bbded212cd1cc329387da9630fc648c24
sohail005 <75928841+sohail005@users.noreply.github.com>
2021-09-07 16:27:03 +0530

    created a loading animation when api is loading

--- src/features/restaurants/screen/restaurants.screen.js
+++ src/features/restaurants/screen/restaurants.screen.js
@@ -1,33 +1,43 @@
-import React, { useContext }  from "react";
-import {FlatList, SafeAreaView} from 'react-native';
-import { Searchbar } from 'react-native-paper';
-import {RestaurantInfoCard} from '../components/restaurant-info.Card.component';
+import React, { useContext } from "react";
+import { Searchbar } from "react-native-paper";
+import { FlatList } from "react-native";
 import styled from "styled-components/native";
+import { ActivityIndicator, Colors } from "react-native-paper";
+import { SafeArea } from "../../../utility/safe-area.component";
+import { RestaurantInfoCard } from "../components/restaurant-info.Card.component";
 import { Spacer } from "../../../components/spacer/spacer.component";
-import {SafeArea} from "../../../utility/safe-area.component";
-import { RestaurantsContext } from "../../../services/restaurants/restaurants.context";
 
+import { RestaurantsContext } from "../../../services/restaurants/restaurants.context";
 
 const SearchContainer = styled.View`
   padding: ${(props) => props.theme.space[2]};
 `;
-const RestaurantListContainer = styled(SafeAreaView)`
- padding: 16px;
-  backgroundColor:lightgray;
-`;
+
 const RestaurantList = styled(FlatList).attrs({
   contentContainerStyle: {
     padding: 16,
   },
-})`margin-bottom: ${(props) => props.theme.space[2]};
-backgroundColor:lightgray`;
+})``;
 
+const Loading = styled(ActivityIndicator)`
+  margin-left: -30px;
+  margin-top: 30%;
+`;
+const LoadingContainer = styled.View`
+  position: absolute;
+  top: 50%;
+  left: 50%;
+`;
 
 export const RestaurantsScreen = () => {
   const { isLoading, error, restaurants } = useContext(RestaurantsContext);
-  console.log(error);
   return (
     <SafeArea>
+      {isLoading && (
+        <LoadingContainer>
+          <Loading size={50} animating={true} color={Colors.blue300} />
+        </LoadingContainer>
+      )}
       <SearchContainer>
         <Searchbar />
       </SearchContainer>
@@ -45,8 +55,3 @@ export const RestaurantsScreen = () => {
     </SafeArea>
   );
 };
-
-
-  
-   
-  
