c401802a79416111810aa082929bbcd6561e01ed
sohail005 <75928841+sohail005@users.noreply.github.com>
2021-09-08 17:36:47 +0530

    if enterd location is correcct then show the results

--- src/services/restaurants/restaurants.context.js
+++ src/services/restaurants/restaurants.context.js
@@ -1,6 +1,10 @@
-import React, { useState, createContext, useEffect, useMemo } from "react";
+import React, { useState, useContext, createContext, useEffect } from "react";
+import {
+  restaurantsRequest,
+  restaurantsTransform,
+} from "./restaurants.services";
 
-import { restaurantsRequest, restaurantsTransform } from "./restaurants.services";
+import { LocationContext } from "../location/location.context";
 
 export const RestaurantsContext = createContext();
 
@@ -8,15 +12,18 @@ export const RestaurantsContextProvider = ({ children }) => {
   const [restaurants, setRestaurants] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState(null);
+  const { location } = useContext(LocationContext);
 
-  const retrieveRestaurants = () => {
+  const retrieveRestaurants = (loc) => {
     setIsLoading(true);
+    setRestaurants([]);
+
     setTimeout(() => {
-      restaurantsRequest()
+      restaurantsRequest(loc)
         .then(restaurantsTransform)
         .then((results) => {
           setIsLoading(false);
-          setRestaurants(results);
+         setRestaurants(results);
         })
         .catch((err) => {
           setIsLoading(false);
@@ -25,8 +32,12 @@ export const RestaurantsContextProvider = ({ children }) => {
     }, 2000);
   };
   useEffect(() => {
-    retrieveRestaurants();
-  }, []);
+    if (location) {
+      const locationString = `${location.lat},${location.lng}`;
+      retrieveRestaurants(locationString);
+    }
+  }, [location]);
+
   return (
     <RestaurantsContext.Provider
       value={{
