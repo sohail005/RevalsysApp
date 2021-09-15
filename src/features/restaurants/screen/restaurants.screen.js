4624eeccf0c387d10c7fef8a7f89bed2f56fa480
sohail005 <75928841+sohail005@users.noreply.github.com>
2021-09-15 15:47:26 +0530

    created a favourite bar on top

--- src/features/restaurants/screen/restaurants.screen.js
+++ src/features/restaurants/screen/restaurants.screen.js
@@ -1,14 +1,16 @@
-import React, { useContext } from "react";
+
 import { FlatList, TouchableOpacity } from "react-native";
 import styled from "styled-components/native";
 import { ActivityIndicator, Colors } from "react-native-paper";
 import { SafeArea } from "../../../utility/safe-area.component";
 import {RestaurantInfoCard} from "../components/restaurant-info.Card.component";
 import { Spacer } from "../../../components/spacer/spacer.component";
-
+import React, { useContext, useState } from "react";
 import { RestaurantsContext } from "../../../services/restaurants/restaurants.context";
-
+import { FavouritesBar } from "../../../services/favourites/favourites-bar.component";
 import { Search } from "../components/search.component";
+
+import { FavouritesContext } from "../../../services/favourites/favourite.context";
 
 const RestaurantList = styled(FlatList).attrs({
   contentContainerStyle: {
@@ -28,6 +30,8 @@ const LoadingContainer = styled.View`
 
 export const RestaurantsScreen = ({ navigation }) => {
   const { isLoading, restaurants } = useContext(RestaurantsContext);
+  const { favourites } = useContext(FavouritesContext);
+  const [isToggled, setIsToggled] = useState(false);
   return (
     <SafeArea>
       {isLoading && (
@@ -35,7 +39,13 @@ export const RestaurantsScreen = ({ navigation }) => {
           <Loading size={60} animating={true} color={Colors.blueGrey500} />
         </LoadingContainer>
       )}
-        <Search />
+       <Search
+        isFavouritesToggled={isToggled}
+        onFavouritesToggle={() => setIsToggled(!isToggled)}
+      />
+      {isToggled && (
+        <FavouritesBar favourites={favourites} onNavigate={navigation.navigate} />
+      )}
       <RestaurantList
         data={restaurants}
         renderItem={({ item }) => {
